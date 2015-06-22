87e1dc2d073ef92e5102bdab48fdb2ade3dbf195
vagrant <vagrant@homestead>
2015-06-22 08:32:50 +0000

    work on making comment create form reusable for posts and comments

--- resources/views/comment/partials/show.blade.php
+++ resources/views/comment/partials/show.blade.php
@@ -6,9 +6,8 @@
 		<div class="col-xs-2">
 			<button class="btn btn-default btn-xs pull-right" data-toggle="collapse" data-target=".c{{ $comment->id }}">Reply</button>
 		</div>
-		
 	</div>
-	@include('comment.partials.create')
+	@include('comment.partials.create', ['comment_id' => $comment->id])
 	@if (count($comment->comments))
 		<ul class="list-group">
 			@foreach ($comment->comments as $comment)
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,6 +7,8 @@
 <div class="jumbotron">
 	<h1 class="text-center"><a href="/p/{{ $post->id }}">{{ $post->title }}</a></h1>
 	<p>{{ $post->content }}</p>
+	<button class="btn btn-default" data-toggle="collapse" data-target=".p{{ $post->id }}">Reply</button>
+	@include('comment.partials.create', ['post_id' => $post->id])
 </div>
 
 <ul class="list-group">
