4af2bcade8ca9fe42cff4d9a6e9e2cfbc881f587
lawrence <l_wakefield@live.com>
2015-07-04 10:13:53 +1000

    added some more buttons for auth/guest and some styling to go with it

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -7,7 +7,11 @@
 			<p>{{ $comment->content }}</p>
 		</div>
 		<div class="col-xs-2">
-			<button class="btn btn-default btn-xs pull-right" data-toggle="collapse" data-target=".c{{ $comment->id }}">Reply</button>
+            @if (Auth::check())
+                <button class="btn btn-default btn-xs pull-right" data-toggle="collapse" data-target=".c{{ $comment->id }}">Reply</button>
+            @else
+                <a class="btn btn-default btn-xs pull-right" href="/login">Reply</a>
+            @endif
 		</div>
 	</div>
 	@include('comment.create', ['comment_id' => $comment->id, 'score' => $comment->score])
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,15 +5,21 @@
 @section('content')
 
 <div class="jumbotron">
-	<div>
-		<div class="pull-left">
+    <div class="row">
+		<div class="col-xs-1">
 			@include('vote.create', ['action' => "/p/$post->id/vote", 'score' => $post->score])
 		</div>
-		<button class="btn btn-default pull-right" data-toggle="collapse" data-target=".p{{ $post->id }}">Reply</button>
+        <h1 class="col-xs-10 text-center">
+            <a href="/p/{{ $post->id }}">{{ $post->title }}</a>
+        </h1>
+		<div class="col-xs-1">
+            @if (Auth::check())
+                <button class="btn btn-default pull-right" data-toggle="collapse" data-target=".p{{ $post->id }}">Reply</button>
+            @else
+                <a class="btn btn-default pull-right" href="/login">Reply</a>
+            @endif
+        </div>
 	</div>
-	<h1 class="text-center">
-		<a href="/p/{{ $post->id }}">{{ $post->title }}</a>
-	</h1>
 	<p>{{ $post->content }}</p>
 	
 	@include('comment.create', ['post_id' => $post->id])
